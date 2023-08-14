e6f68ec4abb2f9fe28064555321289492c9eb78e
Corey Sawin <sawincp@gmail.com>
2023-08-14 13:16:44 -0400

    commit tree 58c9ba3cefac05d65cf62d8a5bbbf70de5fa49f4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,11 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+
   end
 
   private
@@ -19,5 +20,6 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+  
 
 end
